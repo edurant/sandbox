cfe80c91a77ea0943896702dfde2903eb8f71ee3
Eric Durant <eric.durant@gmail.com>
2021-10-04 17:02:33 -0500

    update convolution illustration to pure, modern (R2021b) MATLAB; doc next steps

--- illustrateConvolution.m
+++ illustrateConvolution.m
@@ -1,44 +1,68 @@
-% Base for updates. Original version taken from code as of 2021-10-04 at
+% Create an animated GIF to illustrate convolution of rectangular pulse
+% with causal exponential decay.
+
+% Eric Durant <eric.durant@gmail.com> <https://durant.io/>
+% Heavily modified version (MATLAB 2021b updates, pure MATLAB implementation, and more) of
 % https://commons.wikimedia.org/wiki/File:Convolution_of_spiky_function_with_box2.gif
-% Create folding of rectangular impulse with spiky function
+% fetched on 2021-10-04
 
-clear
+% TODO:
+% Add option to live render in MATLAB (and disable GIF generation)
+% Fix colormap changing each frame
+% Increase rendering resolution
+% Change function notation to standard EE signals notation (and MSOE EE3032)
+% Support other functions / time supports
 
-X=-2.1:0.001:4;
-F1=exp(-X);
-F1(X<0)=0;
-F2=abs(X)<=0.5;
+function illustrateConvolution()
+
+fileName = "conv_box_spike"+".gif";
+
+t = -2.1 : 0.001 : 4;
+F1 = exp(-t);
+F1(t<0) = 0;
+F2 = abs(t)<=0.5;
 clf
 
-mkdir('tmp');
-[tmp zero_offset] = min(abs(X));
+[~, zero_offset] = min(abs(t));
 
-SyncFrames=[1 round(20.3366666*(1:numel(X)))];
-frame=1;
-integral=nan(size(X));
-for offset_i=1:numel(X);
-  offset=X(offset_i);
-  shift=offset_i-zero_offset;
-  F2_shifted = circshift(F2, [0 shift]);
-  product = F2_shifted.*F1;
-  integral(offset_i) = sum(product)/numel(X)*(X(end)-X(1));
+frameRate = 20.3366666; % hard-coded constant without explanation in original
+SyncFrames = [1 round(frameRate*(1:length(t)))];
+frame = 1;
+integral = nan(size(t));
+for offset_i = 1:length(t)
+    offset = t(offset_i);
+    shift = offset_i-zero_offset;
+    F2_shifted = circshift(F2, [0 shift]);
+    product = F2_shifted.*F1;
+    integral(offset_i) = sum(product)/length(t)*(t(end)-t(1));
 
     if offset_i==SyncFrames(frame)
-        frame=frame+1;
-        area(X, product, 'facecolor', 'yellow');
+        frame = frame+1;
+        area(t, product, 'facecolor', 'yellow');
         hold on
-        plot(X, F1, 'b', X, F2_shifted, 'r', X, integral, 'k', [offset offset], [0 2], 'k:')
+        plot(t, F1, 'b', t, F2_shifted, 'r', t, integral, 'k', [offset offset], [0 2], 'k:')
         hold off
         axis image
         axis([-1.6 3.1 0 1.1])
-        xlabel('\tau & t');
+        xlabel('\tau & t')
         grid on
-        legend('Area under f(\tau)g(t-\tau)', 'f(\tau)', 'g(t-\tau)', '(f\astg)(t)');
-        print('-dpng','-r72',sprintf('tmp/conv_box_spike_%06d.png', offset_i));
-        drawnow
-  end
+        legend('Area under f(\tau)g(t-\tau)', 'f(\tau)', 'g(t-\tau)', '(f\astg)(t)')
+        [frame_image, cmap] = rgb2ind(frame2im(getframe(gcf)), 256);
+
+        % FIXME: This assumes cmap never changes, which is invalid. Improvements options: 1) pass
+        % cmap back to rgb2ind on successive calls to dither to initial map, 2) use a fixed
+        % standard map, 3) compute a globally optimal map post hoc. Note: MATLAB R2021b
+        % documentation shows support for GIF frame append with cmap varying, but this causes a
+        % runtime error that is substantiated on recent MathWorks and reddit posts.
+
+        if frame == 1
+            [H,W] = size(frame_image);
+            frame_anim = NaN(H,W,1,length(SyncFrames)); % 2021b imwrite GIF requires HWCN or HW, not HWN
+        end
+        frame_anim(:,:,1,frame) = frame_image;
+    end
 end
 
-system('"C:\Program Files\ImageMagick-6.6.3-Q16\convert.exe" -layers Optimize -delay 5 tmp/conv_box_spike_*.png conv_box_spike.gif');
-delete('tmp/*');
-rmdir('tmp');
+imwrite(frame_anim, cmap, fileName, 'gif', 'Loopcount', inf, 'DelayTime', 1/frameRate)
+
+end % function
