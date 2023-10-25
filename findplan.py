9fadf58967d8092f20459cb5eec67a2b6404638c
Eric Durant <eric.durant@gmail.com>
2023-10-25 15:48:04 -0500

    updated a Box path to match new practice; various code tweaks

--- findplan.py
+++ findplan.py
@@ -11,6 +11,23 @@ import hashlib
 import pandas as pd
 import pyperclip
 
+def ranged_input(upper_end):
+    """Prompt the user until they enter an int between 0 and argument"""
+    while True:
+        user_input = input(f"Selection (0-{upper_end})? ")
+        try:
+            idx = int(user_input)
+            if 0 <= idx <= upper_end:
+                return idx
+            print(f"Please enter an integer between 0 and {upper_end} inclusive.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+def file_sha224(pth):
+    """Encapsulate file opening, hashing, and closing operations"""
+    with open(pth, 'rb') as file:
+        return hashlib.file_digest(file, "sha224").hexdigest()[-4:]
+
 def main(args):
     """Find all matching advising plans, sort by date, copy user selection to clipboard"""
     # Build the list of plans
@@ -23,11 +40,11 @@ def main(args):
     found_plan = [p for p in found_plan if "courseHistories" not in p]
 
     # Create DataFrame with all plan information
-    data_frame = pd.DataFrame(found_plan, columns=['path'])
-    data_frame['mtime'] = pd.to_datetime([int(os.path.getmtime(pth)) for pth in found_plan],
-        unit='s') # int truncates to whole seconds
-    data_frame['sha224'] = ['…'+hashlib.file_digest(open(pth,'rb'), "sha224")
-        .hexdigest()[-4:] for pth in found_plan]
+    data_frame = pd.DataFrame({
+        'path': found_plan,
+        'mtime': pd.to_datetime([int(os.path.getmtime(pth)) for pth in found_plan], unit='s'),
+        'sha224': ['…' + file_sha224(pth) for pth in found_plan]
+    })
 
     # Filter and sort
     data_frame = data_frame.loc[data_frame.groupby('sha224')['mtime'].idxmin()] # oldest only
@@ -40,8 +57,7 @@ def main(args):
     pd.options.display.max_colwidth = None
     print(data_frame)
 
-    idx = int(input("Selection? ")) # TODO: Validate type & range
-    selected_plan = data_frame.iloc[idx].path
+    selected_plan = data_frame.iloc[ranged_input(data_frame.index.max())].path
 
     print(selected_plan)
     pyperclip.copy(selected_plan)
@@ -53,7 +69,7 @@ if __name__ == "__main__":
     # execute only if run as a script
     plan_path = [
         ['Box', 'EECS-Transition', '_Updated-Advising-Plans'], # PD read-only
-        ['Box', 'EECS Faculty and Staff', 'Advising Plans'], # PD writable
+        ['Box', 'EECS Faculty and Staff', 'Advising Plans [DO NOT UPLOAD HERE]'], # former PD write
         ['Box', 'EECS Advising Plans'], # all-advisor writable, PD readable
         ['Dropbox', 'msoe', 'misc', 'advising', 'specificStudents', 'plans'] # local
     ]
